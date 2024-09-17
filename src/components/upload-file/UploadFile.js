36d329e0adbdef1720fe16bb770352c65ad2e008
Dayana-Wal <dayana.b@veltris.com>
2024-09-17 10:23:06 +0530

    Added capital letters for targetted storage

--- src/components/upload-file/UploadFile.js
+++ src/components/upload-file/UploadFile.js
@@ -40,7 +40,7 @@ export function UploadFile({ show, onHide, userId }) {
 		formData.append('targettedStorage', storageOption)
 
 		try {
-			let aws = storageOption === 'Aws' ? 'Aws' : '';
+			let aws = storageOption === 'AWS' ? 'Aws' : '';
 			const response = await axios.post(`${process.env.REACT_APP_FILE_MANAGEMENT_API}/files/${aws}`, formData, {
 				headers: {
 					'Content-Type': 'multipart/form-data',
@@ -78,9 +78,9 @@ export function UploadFile({ show, onHide, userId }) {
 				onChange={(e) => setStorageOption(e.target.value)}
 				>
 					<option value="">Select storage option</option>
-					<option value="LocalStorage">Local Storage</option>
-					<option value="Aws">AWS S3</option>
-					<option value="Azure">Azure Storage</option>
+					<option value="LOCALSTORAGE">Local Storage</option>
+					<option value="AWS">AWS S3</option>
+					<option value="AZURE">Azure Storage</option>
 				</select>
       </div>
 			<div>
