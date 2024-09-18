2725d37fbb47dbc75e923e12303314e602d289a9
wal-anudeepthi <anudeepthi.k@veltris.com>
2024-09-18 18:12:18 +0530

    ToggleMenu options for File operation

--- src/components/files/ListFiles.js
+++ src/components/files/ListFiles.js
@@ -2,14 +2,21 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import {saveAs} from 'file-saver';
+import { Table } from "react-bootstrap";
+import "./ListFiles.css"
 
 function ListFiles() {
 
   const [files, setFiles] = useState();
+  const [fileId, setFileId] = useState(false)
   const location = useLocation();
   const userId = location.state?.userId || {};
   const userName = location.state?.userName || {};
   const navigate = useNavigate();
+
+  const toggleMenu = (fileId) => {
+    setFileId((prevId)=> prevId === fileId ? null : fileId)
+  }
 
   const getFiles = async() =>{
     try{
@@ -71,48 +78,42 @@ function ListFiles() {
   },[])
 
   return(
-    <div className="container mt-5">
+    <div className="container mt-5 w-75">
       <h1 className="text-center mb-4"> {userName} Files</h1>
       {
         files?.length === 0 ? <p>No Files Found</p> : 
-        <table className="table table-striped table-hover table-bordered">
-        <thead className="bg-dark">
-          <tr>
-            <th>File Name</th>
-            <th>Targetted Storage</th>
-            <th>Open File</th>
-            <th>Delete File</th>
-            <th>Download File</th>
-          </tr>
-        </thead>
-        <tbody>
-          {files?.map((file) => (
-            <tr key={file._id}>
-              <td>{file.fileName}</td>
-              <td>{file.targettedStorage}</td>
-              <td>
-              <button className="btn btn-primary"  onClick={() => handleEditFile(file._id, userId)}>
-                  Open File
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-danger" onClick={()=> handleDeleteFile(file._id)}>
-                  Delete File
-                  <span style={{ marginLeft: '5px' }}></span>
-                  <i className="fas fa-trash-alt"></i>
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-warning" onClick={()=> handleDownloadFile(file._id)}>
-                Download File
-                <span style={{ marginLeft: '5px' }}></span>
-                <i className="fas fa-download"></i>
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <div className="container row">
+          {<Table striped bordered hover >
+            <thead className="bg-dark">
+              <tr>
+                <th>File Name</th>
+                <th>Targetted Storage</th>
+                <th>Options</th>
+              </tr>
+            </thead>
+            <tbody>
+              {files?.map((file) => (
+                <tr key={file._id}>
+                  <td className="col-md-6">{file.fileName}</td>
+                  <td className="col-md-4">{file.targettedStorage}</td>
+                  <td className="col-md-1 position-relative">
+                    <button className="btn" onClick={()=> toggleMenu(file._id)}>
+                      &#8942;
+                    </button>
+                    {fileId === file._id && 
+                      <ul className="list-group list-group-flush position-absolute options">
+                        <li className="list-group-item" onClick={() => handleEditFile(file._id, userId)}>Open</li>
+                        <li className="list-group-item" onClick={() => handleDownloadFile(file._id)}>Download</li>
+                        <li className="list-group-item" onClick={() => handleDeleteFile(file._id)}>Delete</li>
+                      </ul>
+                    }
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table> }
+        </div>
+       
       }
     </div>
   )
